7fe7502e8ac72c14ed23a521c545c55d5f055f2e
ccado <djojananil@gmail.com>
2026-09-08 13:30:58 +0200

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,7 @@
    die RLS-Policies aus supabase.sql. Solange hier Platzhalter stehen, arbeitet
    die Website weiter mit lokalem Browser-Speicher. */
 window.OSG_SUPABASE = {
-  url: '',      // z. B. 'https://abcdefgh.supabase.co'
-  anonKey: '',  // z. B. 'eyJhbGciOi...'
+  url: 'https://eymttzjeyqlqrujxzpth.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5bXR0empleXFscXJ1anh6cHRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NjE4ODksImV4cCI6MjEwNDQzNzg4OX0.Sy2mV-YSvvxyJIzPc-Bi-tYP8yxQKRmMgXf1r9fnxfQ',
   bucket: 'medien'
 };
